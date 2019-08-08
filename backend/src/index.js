72ee0a68a9b8113331a215131ac3ee6272693ee7
Thallys Braz <39064364+thallysbraz@users.noreply.github.com>
2019-08-07 22:56:27 -0300

    adicionando banco mongodb

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,7 +1,16 @@
 const express = require("express");
 const morgan = require("morgan");
+const mongoose = require("mongoose");
 
 const app = express();
+
+/** 
+ * Database setup
+*/
+mongoose.connect("mongodb://localhost:27017/upload", {
+    useNewUrlParser: true 
+}),
+
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true}));
